ae6273b57d50776d9f54ba5674ab373716513f26
kannanATexcelforte <kannan@excelforte.com>
2024-05-20 16:34:52 +0530

    20.05.2024
    in app navigation completed

--- projects/getting-started/src/app/product-details/product-details.component.ts
+++ projects/getting-started/src/app/product-details/product-details.component.ts
@@ -15,7 +15,7 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private cartService: CartService
+    // private cartService: CartService
   ) { }
 
   ngOnInit() {
@@ -27,8 +27,8 @@ export class ProductDetailsComponent implements OnInit {
     this.product = products.find(product => product.id === productIdFromRoute);
   }
 
-  addToCart(product: Product) {
-    this.cartService.addToCart(product);
-    window.alert('Your product has been added to the cart!');
-  }
+  // addToCart(product: Product) {
+  //   this.cartService.addToCart(product);
+  //   window.alert('Your product has been added to the cart!');
+  // }
 }
