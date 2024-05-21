8b1cfae1397102481f85318e2a8faa61906b691c
kannanATexcelforte <kannan@excelforte.com>
2024-05-21 11:52:24 +0530

    21.05.2024-ffui
    completed

--- projects/getting-started/src/app/cart/cart.component.ts
+++ projects/getting-started/src/app/cart/cart.component.ts
@@ -12,20 +12,20 @@ export class CartComponent {
 
   items = this.cartService.getItems();
 
-  // checkoutForm = this.formBuilder.group({
-  //   name: '',
-  //   address: ''
-  // });
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
 
   constructor(
     private cartService: CartService,
-    // private formBuilder: FormBuilder,
+    private formBuilder: FormBuilder
   ) {}
 
   onSubmit(): void {
     // Process checkout data here
-    // this.items = this.cartService.clearCart();
-    // console.warn('Your order has been submitted', this.checkoutForm.value);
-    // this.checkoutForm.reset();
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
 }
