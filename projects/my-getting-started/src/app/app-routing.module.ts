18ac05e0c3e314bb2a6db7c05f64e6e8382c6489
kannanATexcelforte <kannan@excelforte.com>
2024-05-23 11:11:51 +0530

    23.05.2024-ianfmgs
    completed

--- projects/my-getting-started/src/app/app-routing.module.ts
+++ projects/my-getting-started/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MobileListComponent } from './mobile-list/mobile-list.component';
+import { MobileDetailsComponent } from './mobile-details/mobile-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MobileListComponent },
+  { path: 'mobiles/:mobileId', component: MobileDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/my-getting-started/src/app/mobile-details/mobile-details.component.ts
+++ projects/my-getting-started/src/app/mobile-details/mobile-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Mobile, mobiles } from '../products';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-mobile-details',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./mobile-details.component.scss']
 })
 export class MobileDetailsComponent {
+  mobile: Mobile | undefined;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const mobileIdFromRoute = Number(routeParams.get('mobileId'));
+    this.mobile = mobiles.find(mobile => mobile.id === mobileIdFromRoute);
+  }
 }
