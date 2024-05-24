ee6dafbb843ed8ee530e8aef4ce65bbce90d30d7
kannanATexcelforte <kannan@excelforte.com>
2024-05-24 15:47:35 +0530

    24.05.2024-an
    completed

--- projects/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ projects/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,25 +10,14 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
-  gotoDetail() {
-    console.log('detail');
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  } 
-
+  }
 }
