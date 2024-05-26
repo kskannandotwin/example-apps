ce91e05cb3ed59a648c33dca60ecb8f265642588
kannanATexcelforte <kannan@excelforte.com>
2024-05-26 14:08:50 +0530

    26.05.2024-gdfs
    completed

--- projects/tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ projects/tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -16,18 +16,27 @@ export class HeroDetailComponent implements OnInit {
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-  ) {}
+  ) { }
   ngOnInit(): void {
     this.getHero();
   }
-  
+
   getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
+
   goBack(): void {
     this.location.back();
   }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }  
 }
+
