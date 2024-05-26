2ec389c498f08938f79f22679a5bf935b82c1286
kannanATexcelforte <kannan@excelforte.com>
2024-05-26 18:24:42 +0530

    26.05.2024-advtt
    completed

--- projects/first-app/src/app/home/home.component.ts
+++ projects/first-app/src/app/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent {
     name: 'Test Home',
     city: 'Test city',
     state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
+    photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
     availableUnits: 99,
     wifi: true,
     laundry: false
