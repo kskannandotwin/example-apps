d126b5f1de26b730e2e48bb891cde5d0225c3ed9
kannanATexcelforte <kannan@excelforte.com>
2024-05-26 18:02:18 +0530

    26.05.2024-aitc
    completed

--- projects/first-app/src/app/housing-location/housing-location.component.ts
+++ projects/first-app/src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./housing-location.component.scss']
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: Housinglocation;
 }
