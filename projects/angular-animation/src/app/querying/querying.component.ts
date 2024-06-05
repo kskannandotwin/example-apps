a8fc2c13c98e956cb2bf0996e82db575a97fd228
kannanATexcelforte <kannan@excelforte.com>
2024-06-05 12:16:05 +0530

    05.06.2024-gsa
    session 3 has completed

--- projects/angular-animation/src/app/querying/querying.component.ts
+++ projects/angular-animation/src/app/querying/querying.component.ts
@@ -1,10 +1,67 @@
+import { animate, animateChild, group, keyframes, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-querying',
   templateUrl: './querying.component.html',
-  styleUrls: ['./querying.component.scss']
+  styleUrls: ['./querying.component.scss'],
+  animations: [
+    trigger('query', [
+      transition(':enter', [
+        style({ height: 0 }),
+        group([
+          animate(500, style({ height: '*' })),
+          query(':enter', [
+            style({ opacity: 0, transform: 'scale(0)' }),
+            animate(2000, style({ opacity: 1, transform: 'scale(1)' }))
+          ]),
+          query('.hero', [
+            style({ transform: 'translateX(-100%)' }),
+            animate('.7s 500ms ease-in', style({ transform: 'translateX(0)' }))
+          ]),
+        ]),
+        query('@animateMe', animateChild()),
+      ]),
+      transition(':leave', [
+        style({ height: '*' }),
+        query('@animateMe', animateChild()),
+        group([
+          animate('500ms 500ms', style({ height: '0', padding: '0' })),
+          query(':leave', [
+            style({ opacity: 1, transform: 'scale(1)' }),
+            animate('1s', style({ opacity: 0, transform: 'scale(0)' }))
+          ]),
+          query('.hero', [
+            style({ transform: 'translateX(0)' }),
+            animate('.7s ease-out', style({ transform: 'translateX(-100%)' }))
+          ]),
+        ]),
+      ]),
+    ]),
+    trigger('animateMe', [
+      transition('* <=> *', animate('500ms cubic-bezier(.68,-0.73,.26,1.65)', keyframes([
+        style({
+          backgroundColor: 'transparent',
+          color: '*',
+          offset: 0
+        }),
+        style({
+          backgroundColor: 'blue',
+          color: 'white',
+          offset: 0.2
+        }),
+        style({
+          backgroundColor: 'transparent',
+          color: '*',
+          offset: 1
+        }),
+      ])))
+    ])
+  ]
 })
 export class QueryingComponent {
-
+  show = true;
+  hero = HEROES[0];
+  toggleDisabled = false;
 }
