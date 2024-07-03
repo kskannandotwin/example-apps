eb5f62604082c7968c3742c544c28c18e72030c8
kannanATexcelforte <kannan@excelforte.com>
2024-07-03 10:13:15 +0530

    03.07.2024-dif
    completed

---
+++ projects/dependency-injection/src/app/heroes/hero.service.5.ts
@@ -0,0 +1,19 @@
+import { EnvironmentInjector, inject, Injectable, runInInjectionContext } from '@angular/core';
+
+@Injectable({providedIn:
+  'root'
+})
+export class SomeService {}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  private environmentInjector = inject(EnvironmentInjector);
+
+  someMethod() {
+    runInInjectionContext(this.environmentInjector, () => {
+      inject(SomeService); // Do what you need with the injected service
+    });
+  }
+}
