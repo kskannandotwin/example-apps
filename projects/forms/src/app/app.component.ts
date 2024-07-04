bc9a05d061bacb85a3c9b802eeb104261425262f
kannanATexcelforte <kannan@excelforte.com>
2024-07-04 14:34:08 +0530

    04.07.2024-tdf
    completed

--- projects/forms/src/app/app.component.ts
+++ projects/forms/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-
-export type EditorType = 'name' | 'profile';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,18 +6,4 @@ export type EditorType = 'name' | 'profile';
 })
 export class AppComponent {
   title = 'forms';
-
-  editor: EditorType = 'name';
-
-  get showNameEditor() {
-    return this.editor === 'name';
-  }
-
-  get showProfileEditor() {
-    return this.editor === 'profile';
-  }
-
-  toggleEditor(type: EditorType) {
-    this.editor = type;
-  }
 }
--- projects/forms/src/app/app.module.ts
+++ projects/forms/src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NameEditorComponent } from './name-editor/name-editor.component';
-import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NameEditorComponent,
-    ProfileEditorComponent
+    HeroFormComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
