047a4962e1e4adfe71278f412c835ae2b2d2493f
kannanATexcelforte <kannan@excelforte.com>
2024-07-04 12:21:44 +0530

    04.07.2024-rf
    completed

--- projects/forms/src/app/app.module.ts
+++ projects/forms/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveModule } from './reactive/reactive.module';
-import { TemplateModule } from './template/template.module';
+import { NameEditorComponent } from './name-editor/name-editor.component';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameEditorComponent,
+    ProfileEditorComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveModule,
-    TemplateModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
