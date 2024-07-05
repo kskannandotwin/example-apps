3c3c25a04ba07e851785a30396408bc8782c0186
kannanATexcelforte <kannan@excelforte.com>
2024-07-05 14:49:25 +0530

    05.07.2024-fm
    completed

--- projects/ng-modules/src/app/app-routing.module.ts
+++ projects/ng-modules/src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'contact', pathMatch: 'full'},
-  { path: 'items', loadChildren: () => import('./items/items.module').then(m => m.ItemsModule) },
-  { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/ng-modules/src/app/app.module.ts
+++ projects/ng-modules/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContactModule } from './contact/contact.module';
-import { GreetingModule } from "./greeting/greeting.module";
+// import the feature module here so you can add it to the imports array below
+import { CustomerDashboardModule } from './customer-dashboard/customer-dashboard.module';
 
 @NgModule({
     declarations: [
         AppComponent
-    ],    
+    ],
     imports: [
-        BrowserModule,  
-        GreetingModule.forRoot({ userName: 'Miss Marple' }),
-        ContactModule,
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        CustomerDashboardModule,
         AppRoutingModule
     ],
     providers: [],
