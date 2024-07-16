5b8641dd05950177939e47cd0aa884521dffa871
kannanATexcelforte <kannan@excelforte.com>
2024-07-16 16:33:02 +0530

    16.07.2024-pcfc
    completed

--- projects/tour-of-heroes/src/app/app.module.ts
+++ projects/tour-of-heroes/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { VillainsComponent } from './villains/villains.component';
+import { VillainDetailComponent } from './villain-detail/villain-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    VillainsComponent,
+    VillainDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/tour-of-heroes/src/app/villains/villains.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { VILLAINS } from '../mock-heroes';
+import { Villain } from '../hero';
+
+@Component({
+  selector: 'app-villains',
+  templateUrl: './villains.component.html',
+  styleUrls: ['./../heroes/heroes.component.scss']
+})
+export class VillainsComponent {
+  villains = VILLAINS;
+  selectedVillain!: Villain;
+
+  onSelect(villain: Villain): void{
+    this.selectedVillain = villain;
+  }
+}
