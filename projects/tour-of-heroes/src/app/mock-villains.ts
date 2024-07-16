a19d7afdbf172bc5d11f2b66f5963332b0038ad1
kannanATexcelforte <kannan@excelforte.com>
2024-07-16 16:13:07 +0530

    16.07.2024-pdsl
    completed

---
+++ projects/tour-of-heroes/src/app/mock-villains.ts
@@ -0,0 +1,8 @@
+import { Villain } from "./hero";
+
+export const VILLAINS: Villain[] = [
+    { id: 11, name: 'Nambiyaar' },
+    { id: 12, name: 'Ashokan' },
+    { id: 13, name: 'Radha Ravi' },
+    { id: 14, name: 'Raguvaran' }
+]
