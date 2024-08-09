d559a18f9b11fec593e33cb0c3de92f16bceee4d
kannanATexcelforte <kannan@excelforte.com>
2024-08-09 12:03:54 +0530

    09.08.2024-pa
    completed

--- projects/working-with-templates/src/app/app.module.ts
+++ projects/working-with-templates/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExampleProgressbarComponent } from './example-progressbar/example-progressbar.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleProgressbarComponent
+    ExampleProgressbarComponent,
+    ProgressBarComponent
   ],
   imports: [
     BrowserModule,
