376ad5a9bede5b9b8d10ae4de11e7d88d2b74082
kannanATexcelforte <kannan@excelforte.com>
2024-08-12 15:51:07 +0530

    12.08.2024-paca
    completed

--- projects/getting-started/src/app/app.module.ts
+++ projects/getting-started/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { EnterLeaveComponent } from './enter-leave/enter-leave.component';
+import { AutoCalculationComponent } from './auto-calculation/auto-calculation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EnterLeaveComponent
+    AutoCalculationComponent
   ],
   imports: [
     BrowserModule,
