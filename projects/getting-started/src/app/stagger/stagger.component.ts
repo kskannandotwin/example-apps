454fe4079dbe1d3708812ec54b2aa1754815e30c
kannanATexcelforte <kannan@excelforte.com>
2024-08-13 12:31:08 +0530

    133.08.2024-psarta
    completed

--- projects/getting-started/src/app/stagger/stagger.component.ts
+++ projects/getting-started/src/app/stagger/stagger.component.ts
@@ -1,27 +1,69 @@
 import { Component } from '@angular/core';
-import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
+import { trigger, transition, style, animate, query, stagger, state } from '@angular/animations';
 
 @Component({
   selector: 'app-stagger',
   templateUrl: './stagger.component.html',
   styleUrls: ['./stagger.component.scss'],
   animations: [
-    trigger('childComponent', [
+    trigger("listAnimation", [
+      transition("* => *", [
+        // each time the binding value changes
+        query(
+          ":leave",
+          [stagger(100, [animate("0.5s", style({ opacity: 0 }))])],
+          { optional: true }
+        ),
+        query(
+          ":enter",
+          [
+            style({ opacity: 0 }),
+            stagger(100, [animate("0.5s", style({ opacity: 1 }))])
+          ],
+          { optional: true }
+        )
+      ])
+    ]),
+    trigger(
+      'enterAnimation', [
       transition(':enter', [
-        animate('2000ms 2s', style({ transform: 'translateX(80%)' })),
-        animate('2000ms 2s', style({ transform: 'translateX(0)' })),
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate('500ms', style({ transform: 'translateX(0)', opacity: 1, 'overflow-x': 'hidden' }))
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateX(0)', opacity: 1 }),
+        animate('500ms', style({ transform: 'translateX(100%)', opacity: 0 }))
+      ])
+    ]
+    ),
+    trigger('slideIn', [
+      state('*', style({ 'overflow-y': 'hidden' })),
+      state('void', style({ 'overflow-y': 'hidden' })),
+      transition('* => void', [
+        style({ height: '*' }),
+        animate(250, style({ height: 0 }))
+      ]),
+      transition('void => *', [
+        style({ height: '0' }),
+        animate(250, style({ height: '*' }))
       ])
     ])
   ]
 })
 export class StaggerComponent {
-  show = true;
-
-  constructor() { }
-
-  ngOnInit() {
-    setTimeout( () => {
-     // this.show = !this.show;
-    },2000);
+  items: ITable[] = [];
+  add() {
+    this.items.push({
+      Name: `Name ${this.items.length + 1}`,
+      Country: `country ${this.items.length + 1}`
+    });
   }
+  remove() {
+    this.items.splice(0, 1);
+  }
+}
+
+export interface ITable {
+  Name: string;
+  Country: string;
 }
