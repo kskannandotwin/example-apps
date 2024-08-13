13948c3e3a061b9b37ecd54737627479ef5eb3fb
kannanATexcelforte <kannan@excelforte.com>
2024-08-13 12:24:11 +0530

    13.08.2024-pssha
    completed

--- projects/getting-started/src/app/app.component.ts
+++ projects/getting-started/src/app/app.component.ts
@@ -1,10 +1,62 @@
+import { animate, animateChild, group, query, sequence, stagger, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+
+const transition1 = transition('* => *', [
+  sequence([
+    group([
+      query('app-child', animateChild({ duration: '0.5s' }), { optional: true })
+    ]),
+    animate('2000ms', style({ transform: 'translateX(80%)' })),
+    animate('2000ms', style({ transform: 'translateX(0)' })),
+  ])
+]);
+
+const trigger1 = trigger('listAnimation', [
+  transition('* => *', [ // each time the binding value changes
+    query(':leave', [
+      stagger(-100, [
+        animate('0.5s', style({ opacity: 0 }))
+      ])
+    ], { optional: true }),
+    query(':enter', [
+      style({ opacity: 0 }),
+      stagger(500, [
+        animate('0.5s', style({ opacity: 1 }))
+      ])
+    ], { optional: true }),
+  ]),
+  transition1
+]);
+
+const trigger2 = trigger('animateChildren', [
+  transition(':enter', [
+    query('app-child', animateChild(), { optional: true })
+  ])
+]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger1,
+    trigger2
+  ]
 })
 export class AppComponent {
   title = 'getting-started';
+
+  items:number[] = [];
+
+  showItems() {
+    this.items = [0, 1, 2, 3, 4];
+  }
+
+  hideItems() {
+    this.items = [];
+  }
+
+  toggle() {
+    this.items.length ? this.hideItems() : this.showItems();
+  }
 }
--- projects/getting-started/src/app/stagger/stagger.component.ts
+++ projects/getting-started/src/app/stagger/stagger.component.ts
@@ -1,63 +1,27 @@
 import { Component } from '@angular/core';
 import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
 
-export const fadeAnimation = trigger('fadeAnimation', [
-  transition(':enter', [
-    style({ opacity: 0 }), animate('300ms', style({ opacity: 1 }))]
-  ),
-  transition(':leave',
-    [style({ opacity: 1 }), animate('300ms', style({ opacity: 0 }))]
-  )
-]);
-
-const listAnimation = trigger('listAnimation', [
-  transition('* <=> *', [
-    query(':enter',
-      [style({ opacity: 0 }), stagger('60ms', animate('600ms ease-out', style({ opacity: 1 })))],
-      { optional: true }
-    ),
-    query(':leave',
-      animate('200ms', style({ opacity: 0 })),
-      { optional: true }
-    )
-  ])
-]);
-
-const images = [
-  'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
-  'https://media.giphy.com/media/6C4y1oxC6182MsyjvK/giphy.gif',
-  'https://media.giphy.com/media/Ov5NiLVXT8JEc/giphy.gif',
-  'https://media.giphy.com/media/SRO0ZwmImic0/giphy.gif',
-  'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif',
-  'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
-  'https://media.giphy.com/media/6C4y1oxC6182MsyjvK/giphy.gif',
-  'https://media.giphy.com/media/Ov5NiLVXT8JEc/giphy.gif',
-  'https://media.giphy.com/media/SRO0ZwmImic0/giphy.gif',
-  'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif',
-  'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
-  'https://media.giphy.com/media/6C4y1oxC6182MsyjvK/giphy.gif',
-  'https://media.giphy.com/media/Ov5NiLVXT8JEc/giphy.gif',
-  'https://media.giphy.com/media/SRO0ZwmImic0/giphy.gif',
-  'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif',
-  'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif'
-];
-
 @Component({
   selector: 'app-stagger',
   templateUrl: './stagger.component.html',
   styleUrls: ['./stagger.component.scss'],
-  animations: [fadeAnimation, listAnimation]
+  animations: [
+    trigger('childComponent', [
+      transition(':enter', [
+        animate('2000ms 2s', style({ transform: 'translateX(80%)' })),
+        animate('2000ms 2s', style({ transform: 'translateX(0)' })),
+      ])
+    ])
+  ]
 })
 export class StaggerComponent {
-  items: any[] = [];
-  images: string[] = [];
   show = true;
 
-  toggleList() {
-    this.items = this.items.length ? [] : [0,1,2,3,4,5,6,7,8,9,10];
-  }
+  constructor() { }
 
-  toggleImages() {
-    this.images = this.images.length ? [] : images;
+  ngOnInit() {
+    setTimeout( () => {
+     // this.show = !this.show;
+    },2000);
   }
 }
